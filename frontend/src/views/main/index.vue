9324a9332bd2938557365ef07ecb11e06ab917f1
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2022-01-15 17:51:16 +0900

    feat: UI component

--- frontend/src/views/main/index.vue
+++ frontend/src/views/main/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="main-container">
     메인내용입니다.
-    <div class="item">아이템</div>
-    <div class="item">아이템</div>
-    <div class="item">아이템</div>
-    <div class="item font-size">아이템</div>
+    <!-- hm-ui 사용법 -->
+    <hm-ui-button label="우리동네 버튼" color="primary" size="w100" @click="onClickButton"></hm-ui-button>
+    <hm-ui-button label="우리동네 버튼" color="white" shadow></hm-ui-button>
+    <hm-ui-icon name="icon-user-bk" @icon-click="onIconClick"></hm-ui-icon>
+    <hm-ui-icon name="icon-back-bk" @icon-click="onIconClick"></hm-ui-icon>
   </div>
 </template>
 <script>
@@ -12,25 +13,19 @@ export default {
   name: 'Main',
   components: {
   },
-  data () {
+  methods: {
+    onClickButton () {
+      alert('button click')
+    },
+    onIconClick () {
+      alert('icon click')
+    }
   }
 }
 </script>
 
 <style lang="scss">
-$color: #333333;
-$color-red: #FF0000;
-
 .main-container {
-  background-color: $color;
-
-  .item {
-    background: $color-red;
-
-    &.font-size {
-      font-size: 50px;
-    }
-  }
 }
 
 </style>
