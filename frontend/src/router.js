4917041aa56f3ae12946cc371f78171c366f2057
김상민 <wrt123123123@gmail.com>
2022-01-15 13:31:59 +0900

    design: change file

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
-import Signup from '../views/Signup.vue'
+import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
 
 Vue.use(VueRouter)
 
