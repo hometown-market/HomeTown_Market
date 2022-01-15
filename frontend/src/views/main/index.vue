1c218bc9f2c46f0263c9cdf7e018459088081d83
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2022-01-15 14:10:24 +0900

    build: Set Scss

--- frontend/src/views/main/index.vue
+++ frontend/src/views/main/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="main-container">
     메인내용입니다.
+    <div class="item">아이템</div>
+    <div class="item">아이템</div>
+    <div class="item">아이템</div>
+    <div class="item font-size">아이템</div>
   </div>
 </template>
 <script>
@@ -12,3 +16,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$color: #333333;
+$color-red: #FF0000;
+
+.main-container {
+  background-color: $color;
+
+  .item {
+    background: $color-red;
+
+    &.font-size {
+      font-size: 50px;
+    }
+  }
+}
+
+</style>
