2d3ac6a78fd07bffb4866ac2ac99e732686f9a8c
suker <suker800@gmail.com>
2022-01-25 15:22:10 +0900

    feat: 찜 기능 수정

--- backend/market/src/main/java/com/example/market/domain/wish/WishRepository.java
+++ backend/market/src/main/java/com/example/market/domain/wish/WishRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
-
-
+    Wish findByProduct_IdAndUser_Id(long product_id, long user_id);
 }
